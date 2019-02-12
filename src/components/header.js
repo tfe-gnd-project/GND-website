717a8c21ce43e36f8f1be505cf31563384812b12
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-12 15:13:14 -0800

    moved styles into header.css

--- src/components/header.js
+++ src/components/header.js
@@ -4,59 +4,13 @@ import { Link } from "gatsby"
 import "./header.css"
 
 
-const headerStyle = {
-  backgroundSize: `100vw`,
-  minHeight: `75px`,
-  backgroundColor: `#0a2f6c`,
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  paddingTop: '10px',
-  paddingLeft: '10px'
-}
-
-const title = {
-  float: `left`, 
-  marginTop: `15px`, 
-  color: 'white', 
-  letterSpacing: '2px',
-  paddingLeft: '10px',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  fontSize: '2.2em',
-  marginLeft: '10px'
-}
-
-const gndlogo = {
-  minHeight: '60px',
-  minWidth: '60px',
-  maxHeight: `60px`, 
-  maxWidth: `60px`, 
-  float: `left`, 
-  margin: `5px`,
-  flexDirection: 'row',
-  flexWrap: 'wrap'
-}
-
-const gndlogobackground = {
-  minHeight: '70px',
-  maxHeight: '70px',
-  minWidth: '70px',
-  maxWidth: '70px',
-  backgroundColor: 'white',
-  borderRadius: '50%',
-  flexDirection: 'row',
-  flexWrap: 'wrap'
-}
-
-
 const Header = ({ siteTitle, menuLinks }) => (
   <div>
-    <div style={headerStyle}>
-        <div style={gndlogobackground}>
-          <img style={gndlogo} src="https://gurunanakdwara.com/img/gnd-logo.svg" alt="logo"/>
+    <div className='headerStyle'>
+        <div className='gndlogobackground'>
+          <img className='gndlogo' src="https://gurunanakdwara.com/img/gnd-logo.svg" alt="logo"/>
         </div>
-        <h1 style={title} >Guru Nanak Dwara</h1>
+        <h1 className='title' >Guru Nanak Dwara</h1>
     </div>
 
     <nav className='menu'>
