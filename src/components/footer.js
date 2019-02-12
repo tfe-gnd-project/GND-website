093d110a4efacf2a7d99a8cc996edf3fbec64eb2
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-12 11:44:20 -0800

    brought back images folder to resolve errors

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+
+const icons = {
+  width: '20px',
+  height: '20px',
+  
+}
+
 const Footer = ({ siteTitle }) => (
   <div style={{ background: `#0a2f6c` }} >
+    {/* <div>
+      <img style ={icons} src="https://cdn3.iconfinder.com/data/icons/transparent-on-dark-grey/500/icon-02-512.png" alt="facebook" />
+    </div> */}
+
     <div style={{ padding: `1.00rem 1.0875rem` }}>
       <div style={{ margin: 0 , color: "white", fontSize: "small", textAlign: 'center'}}>
+      <img style ={icons} src="https://cdn3.iconfinder.com/data/icons/transparent-on-dark-grey/500/icon-02-512.png" alt="facebook" />
       Guru Nanak Dwara is a tax-exempt 501c(3) non-profit charitable organization. Every generous donation is tax deductible in the USA.
       </div>
     </div>
