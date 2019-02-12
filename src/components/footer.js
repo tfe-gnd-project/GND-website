7bf0e4e488dffa8d88e4c240377a8d76853f342e
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-12 14:53:59 -0800

    added social media icons to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,28 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import './footer.css'
+import { FaFacebook, FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa'
+
+
 
 const Footer = ({ siteTitle }) => (
   <div style={{ background: `#0a2f6c` }} >
     <div style={{ padding: `1.00rem 1.0875rem` }}>
-      <div style={{ margin: 0 , color: "white", fontSize: "small", textAlign: 'center'}}>
-      Guru Nanak Dwara is a tax-exempt 501c(3) non-profit charitable organization. Every generous donation is tax deductible in the USA.
+      <div className='font-container'>
+        <a href="https://www.facebook.com/gurunanakdwara/" rel="noopener noreferrer" class="social-media" title="GND Facebook Page" target="_blank"><FaFacebook className='icons'/></a>
+         
+        <a href="https://www.instagram.com/gurunanakdwara/" rel="noopener noreferrer" class="social-media" title="GND Instagram Page" target="_blank">
+        <FaInstagram className='icons' /></a>
+
+        <a href="https://twitter.com/gurunanakdwara" rel="noopener noreferrer"  class="social-media" title="GND Twitter Page" target="_blank">
+        <FaTwitter className='icons' /></a>
+
+        <a href="https://www.youtube.com/c/GuruNanakDwara" rel="noopener noreferrer" class="social-media" title="GND YouTube Channel" target="_blank">
+        <FaYoutube className='icons' /></a>
+      </div>
+
+      <div className='font'>
+        Guru Nanak Dwara is a tax-exempt 501c(3) non-profit charitable organization.
       </div>
     </div>
   </div>
