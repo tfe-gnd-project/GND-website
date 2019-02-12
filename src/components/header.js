8bc05d722c39e484c0063e05dba23ca2479a750f
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-11 16:33:07 -0800

    added icon, title, and menubar

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { Link } from "gatsby"
 
 const headerStyle = {
   backgroundSize: `100vw`,
-  maxHeight: `75px`,
+  minHeight: `75px`,
   backgroundColor: `#0a2f6c`,
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  paddingTop: '10px',
+  paddingLeft: '10px'
 }
 
 const title = {
@@ -12,21 +18,71 @@ const title = {
   marginTop: `15px`, 
   color: 'white', 
   letterSpacing: '2px',
-  paddingLeft: '10px'
+  paddingLeft: '10px',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  fontSize: '2.2em'
 }
 
 const gndlogo = {
-  maxHeight: `50px`, 
-  maxWidth: `50px`, 
+  minHeight: '60px',
+  minWidth: '60px',
+  maxHeight: `60px`, 
+  maxWidth: `60px`, 
   float: `left`, 
-  margin: `10px`,
+  margin: `5px`,
+  flexDirection: 'row',
+  flexWrap: 'wrap'
 }
 
+const gndlogobackground = {
+  minHeight: '70px',
+  maxHeight: '70px',
+  minWidth: '70px',
+  maxWidth: '70px',
+  backgroundColor: 'white',
+  borderRadius: '50%',
+  flexDirection: 'row',
+  flexWrap: 'wrap'
+}
+
+const menu = {
+  padding: '10px',
+  backgroundColor: 'white',
+  display: 'flex',
+  alignItems: 'stretch',
+  justifyContent: 'space-between',
+  width: '100%',
+}
+
+const menufont = {
+  color: `#0a2f6c`,
+  fontFamily: 'sans-serif',
+  textDecoration: 'none',
+  display: 'block',
+  flex: '0 1 auto',
+  paddingLeft: '20px',
+  paddingRight: '20px',
+}
+
+
 const Header = ({ siteTitle }) => (
-  <div style={headerStyle}>
-    <div>
-      <img style={gndlogo} src="https://gurunanakdwara.com/img/gnd-logo.svg" alt="logo"/>
-      <h1 style={title} >GND Website</h1>
+  <div>
+    <div style={headerStyle}>
+        <div style={gndlogobackground}>
+          <img style={gndlogo} src="https://gurunanakdwara.com/img/gnd-logo.svg" alt="logo"/>
+        </div>
+        <h1 style={title} >Guru Nanak Dwara</h1>
+    </div>
+
+    <div style={menu}>
+      <Link to="/" style={menufont}>Home</Link>
+      <Link to="/" style={menufont}>New Here</Link>
+      <Link to="/" style={menufont}>Events</Link>
+      <Link to="/" style={menufont}>Start Serving</Link>
+      <Link to="/" style={menufont}>Photos</Link>
+      <Link to="/" style={menufont}>Donate</Link>            
+      <Link to="/contact/" style={menufont}>Contact</Link>
     </div>
   </div>
 
