09787a570cf903927d2ed23643c7e0cb51ce46d2
Mi Sun Choi <choi.msc033@gmail.com>
2019-02-22 16:26:56 -0800

    added homepage components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Grow from '@material-ui/core/Grow';
-
+import '../assets/index.css'
+import homeimage from '../images/homeimage.png'
 
 class Index extends React.Component {
 
@@ -15,7 +16,11 @@ class Index extends React.Component {
             style={{ transformOrigin: '0 0 0' }}
             {...(true ? { timeout: 1000 } : {})}
         >
-            <div>Welcome to our home page! Visit our Start Serving, Contact, and Donate page!</div>
+            <div className='homeborder'>
+              <div className="homesubheaders">Welcome to Guru Nanak Dwara</div>
+              <img className='image' src={homeimage} alt='service'/><br/>
+              Guru Nanak Dwara, which means "Gate to Guru Nanak," was established in 1970 as the first Gurdwara in the state of Arizona. Today, it continues to serve the community as a vibrant spiritual campus in the center of historic Phoenix. Guru Nanak was the founder and first Guru of Sikhism, and taught that all humans are equal in the eyes of the One Creator.
+            </div>
         </Grow>
       </Layout>
     );
