df25bcb873cdd7b8898095ff168194372460ba64
Mi Sun Choi <choi.msc033@gmail.com>
2019-03-01 13:12:58 -0800

    thank you page for start serving

---
+++ src/pages/startservingthankyou.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class Thankyou extends React.Component {
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="thankyou"/>
+        <center>
+          <div>
+            <img style={{color: `blue`}} src="https://static.thenounproject.com/png/642224-200.png" alt="Community Service" height="200" width="200"/>
+          </div>
+          <h3 style={{fontFamily: 'sans-serif', color: `#0a2f6c`}}>Thank you for volunteering!</h3>
+          <p style={{fontFamily: 'sans-serif', color: `#0a2f6c`}}>You should recieve a confirmation email shortly.</p>
+        </center>
+      </Layout>
+    );
+  }
+}
+
+
+export default Thankyou;
+
