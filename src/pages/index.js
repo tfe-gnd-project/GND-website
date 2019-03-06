27d81e5b9d49fa6c6e40320cd98eaea856a0bf7e
Mi Sun Choi <choi.msc033@gmail.com>
2019-03-06 11:02:46 -0800

    fixed splashimage import issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import '../assets/index.css'
-import homeimage from '../images/homeimage.png'
+import splashimage from '../images/splashimage.png'
 
 class Index extends React.Component {
 
@@ -12,7 +12,7 @@ class Index extends React.Component {
         <SEO title="Home" keywords={[`GND`]} />
             <div className='homeborder'>
               <div className="homesubheaders">Welcome to Guru Nanak Dwara</div>
-              <img className='image' src={homeimage} alt='service'/><br/>
+              <img className='image' src={splashimage} alt='service'/><br/>
               Guru Nanak Dwara, which means "Gate to Guru Nanak," was established in 1970 as the first Gurdwara in the state of Arizona. Today, it continues to serve the community as a vibrant spiritual campus in the center of historic Phoenix. Guru Nanak was the founder and first Guru of Sikhism, and taught that all humans are equal in the eyes of the One Creator.
             </div>
       </Layout>
