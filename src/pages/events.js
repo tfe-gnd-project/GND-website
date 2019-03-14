e65342245aa396bd53e0eb058bf5fa982f7f75f2
Mi Sun Choi <choi.msc033@gmail.com>
2019-03-14 13:43:47 -0700

    added features to events page

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,19 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import FacebookEvents from '../components/events/facebookEvents'
+import GoogleCalendar from '../components/events/googleCalendar'
+import '../assets/events.css'
 
 class Events extends React.Component {
 
   render() {
     return (
       <Layout>
-        <SEO title="Events"/>
-        <div>Events</div>
+        <SEO title="Events"/> 
+        <div className="eventsContainer">     
+          <FacebookEvents />
+          <GoogleCalendar />
+        </div> 
       </Layout>
     );
   }
 }
-
 
 export default Events;
 
